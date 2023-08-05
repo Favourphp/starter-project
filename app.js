45eab1fe9d0c4042634342b77755c464408e6f8d
Anwara Favour <favourchika02@gmail.com>
2023-08-05 20:59:34 +0100

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require ('express')
 const app = express()
 const tasks = require('./routes/tasks')
 require('dotenv').config()
-const notFound = require('./middleware/not-found')
+//const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use('/api/v1/tasks', tasks)
 
 app.use(notFound)
-app.use(errorHandlerMiddleware)
+//app.use(errorHandlerMiddleware)
 
 
 const port =process.env.PORT || 600
