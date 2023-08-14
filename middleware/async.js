52ade8ddcce6e002a57f0c2ff4a06d4126f654b9
Anwara Favour <favourchika02@gmail.com>
2023-08-14 23:06:08 +0100

    updated

--- middleware/async.js
+++ middleware/async.js
@@ -1,14 +1,14 @@
-// const asyncWrapper = (fn) => {
-//     return async (req, res, next) => {
-//       try {
-//         await fn(req, res, next)
-//       } catch (error) {
-//         next(error)
-//       }
-//     }
-//   }
+const asyncWrapper = (fn) => {
+    return async (req, res, next) => {
+      try {
+        await fn(req, res, next)
+       } catch (error) {
+         next(error)
+       }
+    }
+  }
   
-//   module.exports = asyncWrapper
+  module.exports = asyncWrapper
   
 
 
